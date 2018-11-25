1d586ed41c499ecc3632ed3542fff5d4c10b4d5c
William <williamspruyt@hotmail.com>
2018-11-25 13:58:43 +0000

    more collapse

--- js/scripts.js
+++ js/scripts.js
@@ -8,5 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   $(document).ready(function(){
     $('.collapsible').collapsible();
+    var date1 = new Date();
+
+
+var date2 = new Date('May 15, 1973 10:24:00');
+// Sun Dec 17 1995 03:24:00 GMT...
+
+console.log(date1 === date2);
+// expected output: false;
+
+console.log(date1 - date2);
+// expected output: 0
   });
         
