89815e5e524803d9d0df9b67d6967c3d06ff8582
William <williamspruyt@hotmail.com>
2018-11-26 15:01:52 +0000

    age method

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var date1 = new Date();
 
 
-var date2 = new Date('May 15, 1973 10:24:00');
-// Sun Dec 17 1995 03:24:00 GMT...
+var date2 = new Date('May 15, 1973 00:00');
+var daysOld=Math.floor((date1 - date2)/86400000)+" days old";
 
-console.log(date1 === date2);
-// expected output: false;
-
-console.log(date1 - date2);
-// expected output: 0
+ 
+document.getElementById("daysOld").innerHTML = daysOld;
   });
         
