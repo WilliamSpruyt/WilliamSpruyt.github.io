3e99fe0440c3be94d4ee61c802a4a4bcc86a060e
WilliamSpruyt <williamspruyt@hotmail.com>
2019-06-03 11:40:57 +0100

    making adjustments locally

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,14 @@ var daysOld=Math.floor((date1 - date2)/86400000)+" days old";
 document.getElementById("daysOld").innerHTML = daysOld;
   });
         
+function explainDelay(){
+ return confirm("This project is hosted on at a free Heroku domain which 'sleeps' when not in use. You may experience a short delay while it 'wakes up'")
+}
+
+function redirect(url){ 
+   
+  if(explainDelay()){
+   
+    window.location.href=url;
+  }
+}
