a3e9b4735d91e248057f5d63ee5e8b1a828fa729
WilliamSpruyt <williamspruyt@hotmail.com>
2024-04-28 14:41:44 +0100

    working on homepage revamp from local repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,127 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, options);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.collapsible');
+   
+});
+var synth = window.speechSynthesis;
 
-  // Or with jQuery
+var play = document.getElementById('#play');
+var inputTxt = document.querySelector('.txt');
 
-  $(document).ready(function(){
-    $('.collapsible').collapsible();
-    var date1 = new Date();
+var voiceSelect = document.querySelector('select');
+
+var pitch = 1
+var pitchValue = 1
+var rate = 1
+var rateValue = 1
+
+var voices = [];
+
+hour = 0;
+minute = 0;
+
+$(document).ready(function () {
+  $('.collapsible').collapsible();
+  var date1 = new Date();
 
 
-var date2 = new Date('May 15, 1973 00:00');
-var daysOld=Math.floor((date1 - date2)/86400000)+" days old";
+  var date2 = new Date('May 15, 1973 00:00');
+  var daysOld = Math.floor((date1 - date2) / 86400000) + " days old";
+  d = new Date();
+  hour = d.getHours();
+  minute = d.getMinutes();
 
- 
-document.getElementById("daysOld").innerHTML = daysOld;
-  });
-        
-function explainDelay(){
- return confirm("This project is hosted on at a free Heroku domain which 'sleeps' when not in use. You may experience a short delay while it 'wakes up'")
+  MINUTEWORDS = ['bang on', 'just gone', 'about', 'nearly', 'just about']
+  MINUTETIMEWORDS = ['', 'five past', 'ten past', 'a quarter past', 'twenty past', 'twenty-five past', 'half', 'twenty-five to',
+    'twenty to', 'a quarter to', 'ten to', 'five to', '']
+  HOURWORDS = ['midnight', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'noon',
+    'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven'];
+  window.requestAnimationFrame(step);
+
+  document.getElementById("daysOld").innerHTML = daysOld;
+});
+
+
+function step(timestamp) {
+  var d = new Date();
+      
+	  hour=d.getHours();
+	 minute=d.getMinutes();
+	   
+  var theTime=wordTimeMaker(hour,minute);
+if (document.getElementById("timeReadOut").innerHTML !== 'it\'s '+theTime){
+	 
+	$(timeReadOut).removeClass("animated pulse"),console.log(timestamp);
+    setTimeout(function(){ $(timeReadOut).addClass("animated pulse"); }, 10);  
+	
+	 	 
+	
+	
+	}
+document.getElementById("timeReadOut").innerHTML = 'it\'s '+theTime;
+   
+    window.requestAnimationFrame(step);
+  
+}
+ function wordTimeMaker(hora,mins){
+	 var wordTime='';
+	 var h=hora;
+	 
+	 var handPos=Math.round(mins/5);
+	 var handMod=mins%5;
+	 if  (handPos>6){h++}
+	 wordTime=MINUTEWORDS[handMod]+' '+MINUTETIMEWORDS[handPos]+' '+HOURWORDS[h] ;
+	  
+	return wordTime;
+	 
+ }
+ function populateVoiceList() {
+  voices = synth.getVoices();
+  var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  voiceSelect.innerHTML = '';
+  for(i = 0; i < voices.length ; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+  voiceSelect.selectedIndex = selectedIndex;
+}
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
 }
 
-function redirect(url){ 
-   
-  if(explainDelay()){
-   
-    window.location.href=url;
+function speak(){
+	 var d = new Date();
+	  hour=d.getHours();
+	  minute=d.getMinutes();
+  inputTxt='its ' + wordTimeMaker(hour,minute);
+  console.log(inputTxt.value);
+  if(inputTxt.value !== ''){
+    var utterThis = new SpeechSynthesisUtterance(inputTxt);
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for(i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    utterThis.pitch = pitch;
+    utterThis.rate = rate;
+    synth.speak(utterThis);
   }
+}
+
+i.onsubmit = function(event) {
+  event.preventDefault();
+
+  speak();
+
+   
 }
